d16f6e1e0ecb2cc18f05212545707624516e21d6
Yurasevv <yurasev.work@mail.ru>
2023-02-14 16:58:26 +0300

    Minor admin view changes

--- app/admin/views.py
+++ app/admin/views.py
@@ -6,7 +6,9 @@ class AdminLoginView(View):
     async def post(self):
         data = await self.request.json()
         admin = await self.store.admins.get_by_email(data["email"])
-        return json_response(data = {"email =" : admin.email})
+        return json_response(data = {
+            "id" : admin.id,
+            "email" : admin.email})
 
 
 
