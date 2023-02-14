ca6ea4db43432bb74a872281576be8ceeb127d57
Yurasevv <yurasev.work@mail.ru>
2023-02-14 22:24:13 +0300

    Add admin schema

--- app/admin/schemes.py
+++ app/admin/schemes.py
@@ -2,4 +2,6 @@ from marshmallow import Schema, fields
 
 
 class AdminSchema(Schema):
-    pass
+    id = fields.Int(required=False)
+    email = fields.Str(required=True)
+    password = fields.Str(required=True, load_only=True)
