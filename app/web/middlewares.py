b13fe5a14cabdd554df79f5905258b8ee1530f64
Yurasevv <yurasev.work@mail.ru>
2023-02-14 16:59:26 +0300

    Add Exception handling

--- app/web/middlewares.py
+++ app/web/middlewares.py
@@ -1,7 +1,7 @@
 import json
 import typing
 
-from aiohttp.web_exceptions import HTTPUnprocessableEntity
+from aiohttp.web_exceptions import HTTPUnprocessableEntity, HTTPException
 from aiohttp.web_middlewares import middleware
 from aiohttp_apispec import validation_middleware
 
@@ -32,6 +32,18 @@ async def error_handling_middleware(request: "Request", handler):
             status=HTTP_ERROR_CODES[400],
             message=e.reason,
             data=json.loads(e.text),
+        )
+    except HTTPException as e:
+        return error_json_response(
+            http_status=e.status,
+            status=HTTP_ERROR_CODES[e.status],
+            message=str(e)
+        )
+    except Exception as e:
+        return error_json_response(
+            http_status=e.status,
+            status=HTTP_ERROR_CODES[e.status],
+            message=str(e)
         )
     # TODO: обработать все исключения-наследники HTTPException и отдельно Exception, как server error
     #  использовать текст из HTTP_ERROR_CODES
