98cb083920bb15f97d2244fd6bdb660d8cbfd132
Yurasevv <yurasev.work@mail.ru>
2023-02-19 00:26:46 +0300

    Minor changes

--- app/admin/views.py
+++ app/admin/views.py
@@ -1,8 +1,7 @@
 from aiohttp.web_exceptions import HTTPForbidden
 from aiohttp_apispec import request_schema, response_schema
-from aiohttp_session import new_session, get_session
+from aiohttp_session import new_session
 
-from app.admin.models import Admin
 from app.admin.schemes import AdminSchema
 from app.web.app import View
 from app.web.mixins import AuthRequiredMixin
